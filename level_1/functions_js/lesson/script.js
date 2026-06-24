3ec46c6d890514937e8e3ba20d57c3d68644d7ab
Lamuel Wong <lamuel.wong3001@gmail.com>
2026-06-23 21:49:38 -0400

    Added Functions PROJECT and lesson files

---
+++ level_1/functions_js/lesson/script.js
@@ -0,0 +1,55 @@
+// function greet() {
+//     console.log("Hello, welcome to JavaScript!");
+// }
+
+// greet();
+
+// function greetUser(name) {
+//     console.log(`Hello, ${name}!`);
+// };
+
+// greetUser("Alice");
+// greetUser("Bob");
+
+// function addNumbers(num1,  num2, num3) {
+//     console.log(`Sum: ${num1 + num2 + num3}`);
+// }
+
+// addNumbers(1, 2, 3);
+
+// const greet = function(name) {
+//     return `Hello, ${name}`;
+// };
+
+// console.log(greet("Alice"));
+
+// Arrow Function
+// const square = (num) => num ** 2;
+
+// console.log(square(5));
+
+// const multiply = (a, b) =>  {
+//     return a * b;
+// }
+
+// console.log(multiply(3, 4));
+
+// Function Scope and Hoisting
+
+// let globalVar = "I am global";
+
+// function testScope() {
+//     let localVar = "I exist only in this function."
+//     console.log(globalVar);
+//     console.log(localVar);
+// }
+
+// testScope();
+// console.log(globalVar);
+// console.log(localVar);
+
+// hello();
+
+// function hello() {
+//     console.log("Hello from a function declaration");
+// };
---
+++ level_1/functions_js/project/README.md
@@ -0,0 +1,7 @@
+# Interactive Grade Calculator
+
+### Goals
+### Ask the user for three test scores
+### Calculate the average score
+### Assign a letter grade based on the average
+### Display the result in the console
