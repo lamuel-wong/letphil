000fa2f6f6c5d857b6161383ca94602041a5b49e
Lamuel Wong <lamuel.wong3001@gmail.com>
2026-09-01 01:48:46 -0400

    Initial commit

---
+++ level_2/final-project/script.js
@@ -0,0 +1,61 @@
+const scheduleBlocks = [
+  {
+    id: 1,
+    name: "Work",
+    date: "2026-09-01",
+    startTime: "09:15",
+    endTime: "20:30",
+    category: "work",
+  },
+
+  {
+    id: 2,
+    name: "Mentorship Class",
+    date: "2026-09-01",
+    startTime: "22:00",
+    endTime: "23:00",
+    category: "study",
+  },
+
+  {
+    id: 3,
+    name: "Gym",
+    date: "2026-09-02",
+    startTime: "11:00",
+    endTime: "12:15",
+    category: "gym",
+  },
+];
+
+const goals = [
+  {
+    id: 1,
+    name: "Gym",
+    target: 4,
+    progress: 0,
+    unit: "sessions",
+    weekStartDate: "2026-08-30",
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "Study",
+    target: 10,
+    progress: 3.5,
+    unit: "hours",
+    weekStartDate: "2026-08-30",
+    completed: false,
+  },
+];
+
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const contactMessages = [];
